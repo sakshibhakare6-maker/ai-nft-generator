bdf3029890e4fddc35ac15a6fa8ab71c2d6ddc80
sakshibhakare6-maker <sakshibhakare6@gmail.com>
2026-02-24 02:14:34 +0530

    Comment out useState import in Navigation.js
    Commented out the import statement for useState.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-
+//import { useState } from "react";
+import React from 'react';
 const Navigation = ({ account, setAccount }) => {
 
   const connectWallet = async () => {
