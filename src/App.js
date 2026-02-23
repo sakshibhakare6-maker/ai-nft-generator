826acdb2f8c731c8258a94eabd034d0738aba96c
sakshibhakare6-maker <sakshibhakare6@gmail.com>
2026-02-24 02:15:56 +0530

    Disable eslint rules for unused vars and hooks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { NFTStorage } from 'nft.storage'
